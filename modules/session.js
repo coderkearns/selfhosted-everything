3ff7b2fe139329f87739e6ff9c15efa14f971e87
coderkearns <coder.kearns@gmail.com>
2023-04-15 21:05:29 -0500

    feat: store express sessions

--- modules/session.js
+++ modules/session.js
@@ -1,10 +1,12 @@
 const session = require("express-session")
+const FileStore = require("session-file-store")(session)
 
 function useSession(app) {
     app.set("trust proxy", 1)
     app.use(session({
+        store: FileStore(),
         secret: "1d2cee1bb8edef110bf75d1576a613fd",
-        resave: false,
+        resave: true,
         saveUninitialized: true,
     }))
 }
