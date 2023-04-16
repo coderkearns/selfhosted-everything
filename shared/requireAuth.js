aeb4c0719a1ea1054f86a88032ee7e0d9b7d9f99
coderkearns <coder.kearns@gmail.com>
2023-04-15 20:57:19 -0500

    fix: redirect login to correct url

--- shared/requireAuth.js
+++ shared/requireAuth.js
@@ -2,7 +2,7 @@ function requireAuth(req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        res.redirect(`/login?redirect=${encodeURIComponent(req.url)}`)
+        res.redirect(`/login?redirect=${encodeURIComponent(req.originalUrl)}`)
     }
 }
 
